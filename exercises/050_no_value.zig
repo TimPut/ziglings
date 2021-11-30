3339995ff69a5cc4bd2cf954fde2a227b6c05c49
timput <tim@timput.com>
2021-11-30 13:27:18 -0700

    progress checkpoint

--- exercises/050_no_value.zig
+++ exercises/050_no_value.zig
@@ -55,6 +55,7 @@
 //   * undefined - there is no value YET, this cannot be read YET
 //   * null      - there is an explicit value of "no value"
 //   * errors    - there is no value because something went wrong
+
 //   * void      - there will NEVER be a value stored here
 //
 // Please use the correct "no value" for each ??? to make this program
@@ -65,10 +66,10 @@ const std = @import("std");
 const Err = error{Cthulhu};
 
 pub fn main() void {
-    var first_line1: *const [16]u8 = ???;
+    var first_line1: *const [16]u8 = undefined;
     first_line1 = "That is not dead";
 
-    var first_line2: Err!*const [21]u8 = ???;
+    var first_line2: Err!*const [21]u8 = Err.Cthulhu;
     first_line2 = "which can eternal lie";
 
     std.debug.print("{s} {s} / ", .{ first_line1, first_line2 });
@@ -76,8 +77,8 @@ pub fn main() void {
     printSecondLine();
 }
 
-fn printSecondLine() ??? {
-    var second_line2: ?*const [18]u8 = ???;
+fn printSecondLine() void {
+    var second_line2: ?*const [18]u8 = null;
     second_line2 = "even death may die";
 
     std.debug.print("And with strange aeons {s}.\n", .{second_line2.?});
